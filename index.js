314de0796415d834fb993880b53fc3d70b0fce8f
Srirupa Datta <srirupadatta@pursuit.org>
2019-01-04 21:04:45 -0500

    app server now listening for requests

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+// Import Express Library
 const express = require('express');
+
+// Initiate an Express App
 const app = express();
+
+// Define a Port
 const port = 8000;
-const file = require(`./routes/ReadWriteLoad.js`);
+
+// Import Functionalities
+const file = require(`./services/ReadWriteLoad.js`);
+
+// Define Server's Routes
+app.get('/', (req, res) => {
+    res.send('<h1>This is the Homepage</h1>');
+})
+
+
+// Get App Server to listen for requests
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
 });
-const Me = {
-    name: 'Srirupa',
-    age: 22,
-}
-file.save(me, (err, res) => {
-    console.log('hello');
-})
